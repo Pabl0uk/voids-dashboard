e2911b6e66744c8e0b58bb70518f41041e63fcce
Pete Irvine <pete.irvine89@gmail.com>
2025-06-28 00:08:17 +0100

    Fix: Add missing Form import

--- src/app/submissions/page.js
+++ src/app/submissions/page.js
@@ -4,6 +4,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
+import { Form } from 'react-bootstrap';
 import { collection, getDocs } from 'firebase/firestore';
 import { db } from '../lib/firebase';
 
